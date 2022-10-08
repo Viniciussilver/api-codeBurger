5dc1062088949ff6bf7bb25bcdaa4db8a81ce1ad
Viniciussilver <viniciussilverk@gmail.com>
2022-10-08 20:33:29 -0300

    auth token

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -5,7 +5,7 @@ export default (request, response, next) => {
   const authToken = request.headers.authorization
 
   if (!authToken) {
-    return response.status(401).json({ error: "token error" })
+    return response.status(401).json({ error: "Token not provided" })
   }
 
   const token = authToken.split(" ")[1]
@@ -18,9 +18,10 @@ export default (request, response, next) => {
 
       request.userId = decoded.id
       request.userName = decoded.name
+
       return next()
     })
   } catch (err) {
-    return response.status(401).json({ error: "token is invalid" })
+    return response.status(401).json({ error: "Token is invalid" })
   }
 }
